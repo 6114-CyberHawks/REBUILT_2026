7aad42e6654236dc203bce9fc3d06666c77c19fe
TallGuyCode <129175192+TallGuyCode@users.noreply.github.com>
2026-03-03 15:46:18 -0500

    Update IntakePivot CAN ID and command bindings
    Updated CAN ID for IntakePivot and modified button commands to use PID control.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,14 +1,15 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.IntakePivot;
 
 public class RobotContainer {
     // Subsystem
-    private final IntakePivot m_intakePivot = new IntakePivot(16, 11);
+    private final IntakePivot m_intakePivot = new IntakePivot(10, 11);  // Update CAN IDs
     
-    // Xbox controller on port 0
+    // Xbox controller
     private final CommandXboxController m_controller = new CommandXboxController(0);
     
     public RobotContainer() {
@@ -16,26 +17,26 @@ public class RobotContainer {
     }
     
     private void configureBindings() {
-        // A button - Deploy intake (down)
+        // A button - Deploy intake (PID controlled)
         m_controller.a()
-            .whileTrue(Commands.run(() -> {
-                System.out.println("A button pressed - deploying!");
-                m_intakePivot.deployIntake();
-            }, m_intakePivot))
-            .onFalse(Commands.runOnce(() -> {
-                System.out.println("A button released - stopping!");
-                m_intakePivot.stop();
-            }, m_intakePivot));
+            .onTrue(Commands.runOnce(() -> m_intakePivot.deployIntake(), m_intakePivot));
         
-        // Y button - Stow intake (up)
+        // Y button - Stow intake (PID controlled)
         m_controller.y()
-            .whileTrue(Commands.run(() -> {
-                System.out.println("Y button pressed - stowing!");
-                m_intakePivot.stowIntake();
-            }, m_intakePivot))
-            .onFalse(Commands.runOnce(() -> {
-                System.out.println("Y button released - stopping!");
-                m_intakePivot.stop();
-            }, m_intakePivot));
+            .onTrue(Commands.runOnce(() -> m_intakePivot.stowIntake(), m_intakePivot));
+    }
+    
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand() {
+        // Example autonomous command: deploy intake, wait 2 seconds, stow intake
+        return Commands.sequence(
+            Commands.runOnce(() -> m_intakePivot.deployIntake(), m_intakePivot),
+            Commands.waitSeconds(2.0),
+            Commands.runOnce(() -> m_intakePivot.stowIntake(), m_intakePivot)
+        );
     }
 }
