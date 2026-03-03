7e7cbd68564342a12ac34918450fc1155ae9ea37
TallGuyCode <129175192+TallGuyCode@users.noreply.github.com>
2026-03-03 07:06:07 -0500

    First commi

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,56 +1,41 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.subsystems.IntakePivot;  // Import your subsystem
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.IntakePivot;
 
 public class RobotContainer {
-    // Create the subsystem
-    private final IntakePivot m_intakePivot = new IntakePivot(10, 11);
+    // Subsystem
+    private final IntakePivot m_intakePivot = new IntakePivot(16, 11);
     
-    // Button board on USB port 1
-    private final Joystick m_buttonBoard = new Joystick(1);
-    
-    // Buttons
-    private final JoystickButton m_deployButton = new JoystickButton(m_buttonBoard, 1);
-    private final JoystickButton m_stowButton = new JoystickButton(m_buttonBoard, 2);
+    // Xbox controller on port 0
+    private final CommandXboxController m_controller = new CommandXboxController(0);
     
     public RobotContainer() {
         configureBindings();
     }
     
     private void configureBindings() {
-    // Deploy button with debug output
-    m_deployButton
-        .whileTrue(Commands.run(() -> {
-            System.out.println("Button 1 pressed - deploying!");
-            m_intakePivot.deployIntake();
-        }, m_intakePivot))
-        .onFalse(Commands.runOnce(() -> {
-            System.out.println("Button 1 released - stopping!");
-            m_intakePivot.stop();
-        }, m_intakePivot));
-    
-    // Stow button with debug output
-    m_stowButton
-        .whileTrue(Commands.run(() -> {
-            System.out.println("Button 2 pressed - stowing!");
-            m_intakePivot.stowIntake();
-        }, m_intakePivot))
-        .onFalse(Commands.runOnce(() -> {
-            System.out.println("Button 2 released - stopping!");
-            m_intakePivot.stop();
-        }, m_intakePivot));
-}
-
-    
-    public IntakePivot getIntakePivot() {
-        return m_intakePivot;
+        // A button - Deploy intake (down)
+        m_controller.a()
+            .whileTrue(Commands.run(() -> {
+                System.out.println("A button pressed - deploying!");
+                m_intakePivot.deployIntake();
+            }, m_intakePivot))
+            .onFalse(Commands.runOnce(() -> {
+                System.out.println("A button released - stopping!");
+                m_intakePivot.stop();
+            }, m_intakePivot));
+        
+        // Y button - Stow intake (up)
+        m_controller.y()
+            .whileTrue(Commands.run(() -> {
+                System.out.println("Y button pressed - stowing!");
+                m_intakePivot.stowIntake();
+            }, m_intakePivot))
+            .onFalse(Commands.runOnce(() -> {
+                System.out.println("Y button released - stopping!");
+                m_intakePivot.stop();
+            }, m_intakePivot));
     }
 }
-
--- src/main/java/frc/robot/subsystems/IntakePivot.java
+++ src/main/java/frc/robot/subsystems/IntakePivot.java
@@ -1,16 +1,12 @@
-
 package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkMax;
-//import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-//import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.SparkSim;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.ResetMode;
 import com.revrobotics.PersistMode;
-
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.RobotBase;
 
@@ -33,8 +29,11 @@ public class IntakePivot extends SubsystemBase {
     private static final double REVERSE_SOFT_LIMIT = -0.01;
     
     // Motor speeds
-    private static final double DEPLOY_SPEED = 0.3;
-    private static final double STOW_SPEED = 0.5;
+    private static final double DEPLOY_SPEED = 0.1;
+    private static final double STOW_SPEED = 0.1;
+    
+    // Holding voltage to fight gravity when stopped
+    private static final double HOLD_VOLTAGE = -0.08;  // Tune this value (start small!)
     
     // Simulation objects
     private SparkSim m_leaderSim;
@@ -69,54 +68,55 @@ public class IntakePivot extends SubsystemBase {
         
         leaderConfig.smartCurrentLimit(40);
         leaderConfig.idleMode(SparkMaxConfig.IdleMode.kBrake);
-        leaderConfig.inverted(true);
+        leaderConfig.inverted(true);  // Adjust based on your testing
         
         m_leaderMotor.configure(
-            leaderConfig,
-            ResetMode.kResetSafeParameters,
-            PersistMode.kPersistParameters);
+            leaderConfig, 
+            ResetMode.kResetSafeParameters, 
+            PersistMode.kPersistParameters
+        );
         
         SparkMaxConfig followerConfig = new SparkMaxConfig();
         followerConfig.follow(m_leaderMotor);
         followerConfig.smartCurrentLimit(40);
         followerConfig.idleMode(SparkMaxConfig.IdleMode.kBrake);
         
-        m_followerMotor.configure(followerConfig, ResetMode.kResetSafeParameters,
-                                 PersistMode.kPersistParameters);
+        m_followerMotor.configure(
+            followerConfig, 
+            ResetMode.kResetSafeParameters, 
+            PersistMode.kPersistParameters
+        );
         
+        // Zero encoder at startup - IMPORTANT: Arm must be stowed (up) when robot starts!
         m_leaderMotor.getEncoder().setPosition(0.0);
     }
     
     private void setupSimulation() {
         m_leaderSim = new SparkSim(m_leaderMotor, DCMotor.getNEO(2));
-        // Arm physical parameters
-        double armMassKg = 2.04263;              // Estimated arm mass
-        double armLengthMeters = 0.310;        // Estimated arm length
         
-        // Calculate moment of inertia (MOI) for rod rotating about one end
+        double armMassKg = 2.0;
+        double armLengthMeters = 0.5;
         double moiKgMetersSquared = armMassKg * Math.pow(armLengthMeters, 2) / 3.0;
         
-        // Create arm simulator with correct constructor【toolu_vrtx_011emFoS42wdZ9LX53oNMprg-2】
         m_armSim = new SingleJointedArmSim(
-            DCMotor.getNEO(2),                  // Gearbox: 2 NEO motors
-            23.0,                               // Gear ratio: 23:1
-            moiKgMetersSquared,                 // Moment of inertia (kg*m^2)
-            armLengthMeters,                    // Arm length (meters)
-            Units.degreesToRadians(0),          // Min angle (radians)
-            Units.degreesToRadians(93),         // Max angle (radians)
-            true,                               // Simulate gravity
-            Units.degreesToRadians(0)           // Starting angle (radians)
+            DCMotor.getNEO(2),
+            23.0,
+            moiKgMetersSquared,
+            armLengthMeters,
+            Units.degreesToRadians(0),
+            Units.degreesToRadians(93),
+            true,
+            Units.degreesToRadians(0)
         );
         
-        // Create 2D mechanism visualization
         m_mech2d = new Mechanism2d(2, 2);
         MechanismRoot2d root = m_mech2d.getRoot("IntakePivot", 1, 0.5);
         m_armVisual = root.append(
             new MechanismLigament2d(
                 "Intake Arm",
                 armLengthMeters,
-                90,                             // Starting angle (degrees)
-                6,                              // Line width
+                90,
+                6,
                 new Color8Bit(Color.kBlue)
             )
         );
@@ -140,28 +140,19 @@ public class IntakePivot extends SubsystemBase {
     
     @Override
     public void simulationPeriodic() {
-        // Get motor voltage
         double voltage = m_leaderSim.getAppliedOutput() * 12.0;
         
-        // Update simulation
         m_armSim.setInputVoltage(voltage);
-        m_armSim.update(0.02);  // 20ms timestep
+        m_armSim.update(0.02);
         
-        //Get simulated velocity
         double velocityRadPerSec = m_armSim.getVelocityRadPerSec();
-
-        //Update SparkMax sim with velocity
         m_leaderSim.iterate(velocityRadPerSec, 12.0, 0.02);
-     
         
-        // Get simulated position
         double angleRadians = m_armSim.getAngleRads();
         m_simPosition = angleRadians / (2 * Math.PI);
         
-        // Update visual (90 degree offset for display)
         m_armVisual.setAngle(90 - Units.radiansToDegrees(angleRadians));
         
-        // Publish simulation data
         SmartDashboard.putNumber("IntakePivot/Sim Angle (deg)", 
             Units.radiansToDegrees(angleRadians));
         SmartDashboard.putNumber("IntakePivot/Sim Current (A)", 
@@ -169,15 +160,19 @@ public class IntakePivot extends SubsystemBase {
     }
     
     public void deployIntake() {
+        System.out.println("DEPLOY CALLED - Setting speed to " + DEPLOY_SPEED);
         m_leaderMotor.set(DEPLOY_SPEED);
     }
     
     public void stowIntake() {
+        System.out.println("STOW CALLED - Setting speed to " + (-STOW_SPEED));
         m_leaderMotor.set(-STOW_SPEED);
     }
     
     public void stop() {
-        m_leaderMotor.set(0.0);
+        System.out.println("STOP CALLED - Applying hold voltage: " + HOLD_VOLTAGE);
+        // Apply small upward voltage to hold against gravity
+        m_leaderMotor.setVoltage(HOLD_VOLTAGE);
     }
     
     public double getPosition() {
@@ -191,4 +186,3 @@ public class IntakePivot extends SubsystemBase {
         return getPosition() * 360.0;
     }
 }
-
