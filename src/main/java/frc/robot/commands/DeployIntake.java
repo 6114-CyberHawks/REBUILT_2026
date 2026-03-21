808832566b0b53c43efab3f615dc6c2069907238
Rustic <94873187+ChromoRust@users.noreply.github.com>
2026-03-21 14:28:46 -0400

    Testin

--- src/main/java/frc/robot/commands/DeployIntake.java
+++ src/main/java/frc/robot/commands/DeployIntake.java
@@ -30,7 +30,7 @@ public class DeployIntake extends Command {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    intakeSubsystem.StopIntakePivot();
+    // intakeSubsystem.StopIntakePivot();
   }
 
   // Returns true when the command should end.
